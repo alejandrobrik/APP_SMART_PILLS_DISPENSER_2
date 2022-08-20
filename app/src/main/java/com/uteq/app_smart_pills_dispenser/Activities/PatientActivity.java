76e09b279eca009c17842032905a883f8dfd4524
Jonathan cevallos Mayorga <45270300+JonathanCevallos@users.noreply.github.com>
2022-08-19 21:47:27 -0500

    commit new

--- app/src/main/java/com/uteq/app_smart_pills_dispenser/Activities/PatientActivity.java
+++ app/src/main/java/com/uteq/app_smart_pills_dispenser/Activities/PatientActivity.java
@@ -1,16 +1,33 @@
 package com.uteq.app_smart_pills_dispenser.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.uteq.app_smart_pills_dispenser.R;
+import com.uteq.app_smart_pills_dispenser.models.Patient;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class PatientActivity extends AppCompatActivity {
+
+    SearchView txtBuscar;
+    RecyclerView listaPacientes;
+    ArrayList<Patient>listaArraPacientes;
+    FloatingActionButton  fabNuevo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+        txtBuscar = findViewById(R.id.txtBuscar);
+        listaPacientes.setLayoutManager(new LinearLayoutManager(this));
+        fabNuevo = findViewById(R.id.favNuevo);
+
     }
 }
