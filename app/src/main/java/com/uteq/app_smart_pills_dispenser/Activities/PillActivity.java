0e14ca4fff2fcc26f1c3bfb5c62ce0907e431761
Lin2808 <linjaviers@hotmail.com>
2022-08-20 20:26:39 -0500

    Agregado el registrar Píldoras

---
+++ app/src/main/java/com/uteq/app_smart_pills_dispenser/Activities/PillActivity.java
@@ -0,0 +1,14 @@
+package com.uteq.app_smart_pills_dispenser.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pill);
+    }
+}
