dc77c05e79bc9d62c69f9fd304fae755e358156f
Lin2808 <linjaviers@hotmail.com>
2022-08-28 12:29:44 -0500

    Nuevos archivos

--- app/src/main/java/com/uteq/app_smart_pills_dispenser/ui/pills/PillAddFragment.java
+++ app/src/main/java/com/uteq/app_smart_pills_dispenser/ui/pills/PillAddFragment.java
@@ -1,11 +1,7 @@
 package com.uteq.app_smart_pills_dispenser.ui.pills;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,25 +10,57 @@ import android.view.ViewGroup;
 
 import com.uteq.app_smart_pills_dispenser.R;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PillAddFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class PillAddFragment extends Fragment {
 
-    private PillAddViewModel mViewModel;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-    public static PillAddFragment newInstance() {
-        return new PillAddFragment();
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public PillAddFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment PillAddFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static PillAddFragment newInstance(String param1, String param2) {
+        PillAddFragment fragment = new PillAddFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pill_add, container, false);
     }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PillAddViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/uteq/app_smart_pills_dispenser/ui/pills/PillAddViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.uteq.app_smart_pills_dispenser.ui.pills;
-
-import androidx.lifecycle.ViewModel;
-
-public class PillAddViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
